fb666ce073ec4188a51bdd097c9a561042d16ed0
PradipKhamkar <khamkarpradip20@gmail.com>
2024-07-27 03:37:25 -0400

    added Routes 🛣

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,14 +4,25 @@ import Home from "./Views/Home";
 import Header from "./Components/Layouts/Header";
 import "./index.css";
 import Sidebar from "./Components/Layouts/Sidebar";
+import { BrowserRouter, Router, Routes, Route } from "react-router-dom";
+import AllNotes from "./Views/Notes/AllNotes";
+import CreateNote from "./Views/Notes/CreateNote";
 
 const App = () => {
   return (
-    <NextUIProvider className="bg-[#f8f8fa] h-screen">
-      {/* <Header /> */}
-      {/* <Home /> */}
-      <Sidebar />
-    </NextUIProvider>
+    <>
+      <NextUIProvider className="bg-[#f8f8fa] h-screen">
+        <BrowserRouter>
+          <Routes>
+            <Route element={<Home />} path="/" loader>
+              <Route element={<AllNotes />} path="/" />
+              <Route element={<CreateNote />} path="/new" />
+              <Route element={<p>NO PAGE FOUND</p>} path="*" />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </NextUIProvider>
+    </>
   );
 };
 
--- client/src/Views/Home.tsx
+++ client/src/Views/Home.tsx
@@ -1,8 +1,18 @@
 import React from "react";
+import Sidebar from "../Components/Layouts/Sidebar";
 import Wrapper from "../Components/Layouts/Wrapper";
-
+import { Outlet } from "react-router-dom";
 const Home = () => {
-  return <Wrapper></Wrapper>;
+  return (
+    <Wrapper>
+      <div className="flex gap-3">
+        <Sidebar />
+        <div className="bg-white !w-full !max-h-[95vh] overflow-auto rounded-lg">
+          <Outlet />
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Home;
