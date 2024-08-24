53ba0dd010e410af01e6b6c2fe9a32bb41934bf4
PradipKhamkar <khamkarpradip20@gmail.com>
2024-08-24 01:53:15 -0400

    added note model

---
+++ backend/app/models/note.models.ts
@@ -0,0 +1,42 @@
+import { Schema, model } from "mongoose";
+import { INOTE, INOTECONFIG } from "./types/types";
+
+const noteSchema = new Schema<INOTE>(
+  {
+    title: {
+      type: String,
+      required: [true, "title required"],
+      trim: true,
+      lowercase: true,
+      index: true,
+    },
+    data: {
+      type: String,
+      required: [true, "data required"],
+      trim: true,
+      lowercase: true,
+    },
+    config: {
+      type: Object,
+      default: {
+        theme: {
+          color: "",
+          url: "",
+        },
+      },
+    },
+    created_by: {
+      type: Schema.Types.ObjectId,
+      ref: "users",
+    },
+    folder: {
+      type: Schema.Types.ObjectId,
+      ref: "folders",
+    },
+  },
+  { timestamps: true }
+);
+
+const Note = model("note", noteSchema);
+
+export default Note;
--- backend/app/models/types/types.ts
+++ backend/app/models/types/types.ts
@@ -11,3 +11,18 @@ export interface IUSER {
   created_at: Date;
   avatar: IAVATAR;
 }
+
+export interface INOTECONFIG {
+  theme?: {
+    color?: string;
+    url?: string;
+  };
+}
+export interface INOTE {
+  title: string;
+  data: string;
+  pin_at: Date;
+  folder: string | undefined;
+  created_by: string | undefined;
+  config: INOTECONFIG;
+}
