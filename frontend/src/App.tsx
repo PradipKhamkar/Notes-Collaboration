01aa06d2dcff98ed03a867d15ee5a4bb262d7556
PradipKhamkar <khamkarpradip20@gmail.com>
2024-09-14 12:11:21 -0400

    added hugeicons icons ⚡

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,11 @@
+import { Notification03Icon } from "hugeicons-react";
+
 const App = () => {
-  return <div className="text-red-900 bg-green-500">oerm</div>;
+  return (
+    <div className="text-red-900 bg-green-500 text-xs">
+      <Notification03Icon size={20} mode="hgi-duotone hgi-notification-03" />
+    </div>
+  );
 };
 
 export default App;
